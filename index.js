1e9b0d957cece9bc0eefddec5d12adb5a3696109
WSINTRA <will.simpson85@gmail.com>
2019-07-22 12:03:22 -0400

    animated the drawing

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-const animate = () => {
-console.log("scroll")
+const animateHeader = () => {
+
 let headtextForAnimation = document.querySelector('.heading-primary-sub');
 let tailtextForAnimation = document.querySelector('.heading-secondary-sub')
+
 headtextForAnimation.classList.add('animate-scroll');
 tailtextForAnimation.classList.add('animate')
+drawLines()
+}
+let drawIsRun = false
+
+const drawLines = () => {
+	if(drawIsRun === false){
+		let allpath = document.querySelectorAll('path')
+		let length = allpath[0].getTotalLength()
+		console.log(length)
+        allpath.forEach(path => {
+        path.style.transition = path.style.WebkitTransition =
+		  'none';
+		// Set up the starting positions
+		path.style.strokeDasharray = length + ' ' + length;
+		path.style.strokeDashoffset = length;
+		// Trigger a layout so styles are calculated & the browser
+		// picks up the starting position before animating
+		path.getBoundingClientRect();
+		// Define our transition
+		path.style.transition = path.style.WebkitTransition =
+		  'stroke-dashoffset 6s ease-in-out';
+		// Go!
+		path.style.strokeDashoffset = '0';
+        })
+		
+		drawIsRun = true
+	}
+
 }
 
 
+window.addEventListener('scroll', animateHeader )
 
-window.addEventListener('scroll', animate )
